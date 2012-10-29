814dc2f01c3eb7fdd8c26f6defab8ff617a57765
Nikolay Elenkov <nikolay.elenkov@gmail.com>
2012-10-29 23:19:51 +0900

    relaxed exception spec

--- src/com/github/andlyticsproject/console/v2/DevConsoleAuthenticator.java
+++ src/com/github/andlyticsproject/console/v2/DevConsoleAuthenticator.java
@@ -2,7 +2,7 @@ package com.github.andlyticsproject.console.v2;
 
 import android.app.Activity;
 
-import com.github.andlyticsproject.console.AuthenticationException;
+import com.github.andlyticsproject.console.DevConsoleException;
 
 public interface DevConsoleAuthenticator {
 
@@ -11,8 +11,9 @@ public interface DevConsoleAuthenticator {
 	// Activity may be needed to start authentication sub activity (password or
 	// approval prompt, etc). It needs to override onActivityResult() and
 	// retry if RESULT_OK
-	SessionCredentials authenticate(Activity activity, boolean invalidate) throws AuthenticationException;
+	SessionCredentials authenticate(Activity activity, boolean invalidate)
+			throws DevConsoleException;
 
 	// Use this when calling from a service. Won't launch any UIs
-	SessionCredentials authenticateSilently(boolean invalidate) throws AuthenticationException;
+	SessionCredentials authenticateSilently(boolean invalidate) throws DevConsoleException;
 }
