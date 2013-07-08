361abce8482ffe46059e3f3048eec7448fd36f2b
Nikolay Elenkov <nikolay.elenkov@gmail.com>
2013-07-08 17:50:26 +0900

    open DetailsActivity by default

--- src/com/github/andlyticsproject/MainListAdapter.java
+++ src/com/github/andlyticsproject/MainListAdapter.java
@@ -371,8 +371,7 @@ public class MainListAdapter extends BaseAdapter {
 		holder.icon.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				//				Intent intent = new Intent(activity, AppInfoActivity.class);
-				Intent intent = new Intent(activity, DetailsActivity.class);
+				Intent intent = new Intent(activity, AppInfoActivity.class);
 				intent.putExtra(Constants.PACKAGE_NAME_PARCEL, packageName);
 				if (iconFile.exists()) {
 					intent.putExtra(Constants.ICON_FILE_PARCEL, iconFile.getAbsolutePath());
@@ -411,7 +410,7 @@ public class MainListAdapter extends BaseAdapter {
 			@Override
 			public void onClick(View v) {
 
-				Intent intent = new Intent(activity, CommentsActivity.class);
+				Intent intent = new Intent(activity, DetailsActivity.class);
 				intent.putExtra(Constants.PACKAGE_NAME_PARCEL, packageName);
 				if (iconFile.exists()) {
 					intent.putExtra(Constants.ICON_FILE_PARCEL, iconFile.getAbsolutePath());
