ae98d64344f24a7ab455d1e324b38e15cea9b475
Nikolay Elenkov <nikolay.elenkov@gmail.com>
2014-02-05 11:36:33 +0900

    backported OAuth authenticator

--- src/com/github/andlyticsproject/console/v2/BaseAuthenticator.java
+++ src/com/github/andlyticsproject/console/v2/BaseAuthenticator.java
@@ -1,16 +1,31 @@
 package com.github.andlyticsproject.console.v2;
 
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.Builder;
+import android.util.Log;
+
+import com.github.andlyticsproject.AndlyticsApp;
+import com.github.andlyticsproject.R;
+import com.github.andlyticsproject.model.DeveloperConsoleAccount;
+import com.github.andlyticsproject.util.FileUtils;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringEscapeUtils;
-
-import com.github.andlyticsproject.model.DeveloperConsoleAccount;
-
 public abstract class BaseAuthenticator implements DevConsoleAuthenticator {
+
+	private static final String TAG = BaseAuthenticator.class.getSimpleName();
 
 	protected static final Pattern DEV_ACC_PATTERN = Pattern
 			.compile("\"DeveloperConsoleAccounts\":\"\\{\\\\\"1\\\\\":\\[\\{\\\\\"1\\\\\":\\\\\"(\\d{20})\\\\\"");
@@ -70,6 +85,43 @@ public abstract class BaseAuthenticator implements DevConsoleAuthenticator {
 
 	public String getAccountName() {
 		return accountName;
+	}
+
+	protected void debugAuthFailure(Activity activity, String responseStr, String webloginUrl) {
+		FileUtils.writeToAndlyticsDir("console-response.html", responseStr);
+		openAuthUrlInBrowser(activity, webloginUrl);
+	}
+
+	protected void openAuthUrlInBrowser(Activity activity, String webloginUrl) {
+		if (webloginUrl == null) {
+			Log.d(TAG, "Null webloginUrl?");
+			return;
+		}
+
+		Log.d(TAG, "Opening login URL in browser: " + webloginUrl);
+
+		Intent viewInBrowser = new Intent(Intent.ACTION_VIEW);
+		viewInBrowser.setData(Uri.parse(webloginUrl));
+
+		// Always show the notification
+		// When this occurs, it can often occur in batches, e.g. if a the user also clicks to view
+		// comments which results in multiple dev consoles opening in their browser without an
+		// explanation. This is even worse if they have multiple accounts and/or are currently
+		// signed in via a different account
+		Context ctx = AndlyticsApp.getInstance();
+		Builder builder = new NotificationCompat.Builder(ctx);
+		builder.setSmallIcon(R.drawable.statusbar_andlytics);
+		builder.setContentTitle(ctx.getResources().getString(R.string.auth_error, accountName));
+		builder.setContentText(ctx.getResources().getString(R.string.auth_error_open_browser,
+				accountName));
+		builder.setAutoCancel(true);
+		PendingIntent contentIntent = PendingIntent.getActivity(ctx, accountName.hashCode(),
+				viewInBrowser, PendingIntent.FLAG_UPDATE_CURRENT);
+		builder.setContentIntent(contentIntent);
+
+		NotificationManager nm = (NotificationManager) ctx
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+		nm.notify(accountName.hashCode(), builder.build());
 	}
 
 }
